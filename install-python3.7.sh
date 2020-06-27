6e329494bb30948b1653a46d88bc4ade87ceb22e
solidus1983 <8018745+solidus1983@users.noreply.github.com>
2020-06-27 07:28:27 +0100

    Update install-python3.7.sh
    Adding forgotten commands for python3 and python3-pip installs before compiling from source.

--- install-python3.7.sh
+++ install-python3.7.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt install python3 python3-pip
 sudo apt install -y git zlib1g-dev libffi-dev libbz2-dev libncursesw5-dev libgdbm-dev liblzma-dev libsqlite3-dev tk-dev uuid-dev libreadline-dev
 git clone https://github.com/python/cpython.git
 cd cpython/
