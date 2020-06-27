c50f8179eafbccd0708df132f104abab6c50f063
solidus1983 <8018745+solidus1983@users.noreply.github.com>
2020-06-27 03:15:54 +0100

    Create install-python3.7.sh

---
+++ install-python3.7.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt install git zlib1g-dev libffi-dev libbz2-dev libncursesw5-dev libgdbm-dev liblzma-dev libsqlite3-dev tk-dev uuid-dev libreadline-dev
+git clone https://github.com/python/cpython.git
+cd cpython/
+git checkout v3.7.2
+./configure --enable-optimizations
+sudo make && sudo make install
