adb8affb81632dd25f02969ef4b22e54c7f48c91
solidus1983 <8018745+solidus1983@users.noreply.github.com>
2020-06-27 04:26:23 +0100

    Update install-python3.7.sh
    Added the -y switch to apt as not requiring end user input.

--- install-python3.7.sh
+++ install-python3.7.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install git zlib1g-dev libffi-dev libbz2-dev libncursesw5-dev libgdbm-dev liblzma-dev libsqlite3-dev tk-dev uuid-dev libreadline-dev
+sudo apt install -y git zlib1g-dev libffi-dev libbz2-dev libncursesw5-dev libgdbm-dev liblzma-dev libsqlite3-dev tk-dev uuid-dev libreadline-dev
 git clone https://github.com/python/cpython.git
 cd cpython/
 git checkout v3.7.2
