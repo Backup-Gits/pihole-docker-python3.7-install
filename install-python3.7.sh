1245c6df387c135daf95fe506d4a167a9b50c511
solidus1983 <8018745+solidus1983@users.noreply.github.com>
2020-07-05 00:53:46 +0100

    Update install-python3.7.sh

--- install-python3.7.sh
+++ install-python3.7.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install python3 python3-pip
+sudo apt install -y python3 python3-pip
 sudo apt install -y zlib1g-dev libffi-dev libbz2-dev libncursesw5-dev libgdbm-dev liblzma-dev libsqlite3-dev tk-dev uuid-dev libreadline-dev libssl-dev libncurses5-dev libtk8.5 libgdm-dev libdb4o-cil-dev libpcap-dev
 git clone https://github.com/python/cpython.git
 cd cpython/
